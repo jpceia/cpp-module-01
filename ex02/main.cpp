a1249df0a7d9777db039e131c50712ebf511d359
jpceia <joao.p.ceia@gmail.com>
2021-11-01 07:32:15 +0000

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 17:26:31 by jceia             #+#    #+#             */
+/*   Updated: 2021/11/01 07:32:50 by jceia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+    std::string s = "HI THIS IS BRAIN";
+    std::string *stringPTR = &s;
+    std::string &stringREF = s;
+
+    std::cout << "String: " << s << std::endl;
+    std::cout << "Address in memory of the string (using pointer): " << stringPTR << std::endl;
+    std::cout << "Address in memory of the string (using reference): " << &stringREF << std::endl;
+    std::cout << "Displaying string using pointer: " << *stringPTR << std::endl;
+    std::cout << "Displaying string using reference: " << stringREF << std::endl;
+    return 0;
+}
